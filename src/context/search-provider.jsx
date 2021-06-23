e3cad4c7db472f2342b7c1db1e45bba0a8374ba8
DanijelaVegaIT <85353201+DanijelaVegaIT@users.noreply.github.com>
2021-06-23 14:26:40 +0200

    shopify api

--- src/context/search-provider.jsx
+++ src/context/search-provider.jsx
@@ -2,9 +2,10 @@ import * as React from "react"
 import { createClient, Provider as UrlqProvider } from "urql"
 
 const urqlClient = createClient({
-  url: `https://${process.env.GATSBY_SHOPIFY_STORE_URL}/api/2021-01/graphql.json`,
+  url: `https://${process.env.GATSBY_SHOPIFY_STORE_URL}/api/2020-10/graphql.json`,
   fetchOptions: {
     headers: {
+      "Content-Type": "application/json",
       "X-Shopify-Storefront-Access-Token":
         process.env.GATSBY_STOREFRONT_ACCESS_TOKEN,
     },
